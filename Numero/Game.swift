dbf0d7fbc4828fc73f9250aadebbc00eb7acf3b6
EunYeongKim <povoq1024@gmail.com>
2021-04-25 16:58:46 +0900

    Numero 단위 테스트 완성

--- Numero/Game.swift
+++ Numero/Game.swift
@@ -100,12 +100,7 @@ private extension Game {
   }
   
   func generateAnswers(_ index: Int, number: Int) -> String {
-    // TODO: (Final project) Uncomment line below and comment out following line
-    // to use the converter
-    //    let correctAnswer = converter.convert(number)
-    // TODO: (Starter project) Uncomment line below and comment out the previous
-    // line
-    let correctAnswer = "ABCD"
+    let correctAnswer = converter.convert(number)
     if showWrongConversion() {
       isCorrectConversion.append(false)
       return correctAnswer.garbled()
--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -12,10 +12,14 @@ import XCTest
 class ConverterTests: XCTestCase {
     let converter = Converter()
     
+    // 테스트 함수가 실행되기 전에 항상 실행 됨
+    // 초기화 작업
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
+    // 테스트 함수가 실행이 끝난 후 항상 실행 됨
+    // 정리 작업
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
@@ -28,5 +32,46 @@ class ConverterTests: XCTestCase {
     func testConversionForTwo() {
         let result = converter.convert(2)
         XCTAssertEqual(result, "II", "Conversion for 2 is incorrect")
+    }
+    
+    func testConversionForFive() {
+        let result = converter.convert(5)
+        XCTAssertEqual(result, "V", "Conversion for 5 is incorrect")
+    }
+    func testConversionForSix() {
+        let result = converter.convert(6)
+        XCTAssertEqual(result, "VI", "Conversion for 6 is incorrect")
+    }
+    
+    func testConversionForTen() {
+        let result = converter.convert(10)
+        XCTAssertEqual(result, "X", "Conversion for 10 is incorrect")
+    }
+    
+    func testConversionForTwenty() {
+        let result = converter.convert(20)
+        XCTAssertEqual(result, "XX", "Conversion for 20 is incorrect")
+    }
+    
+    // 특별한 케이스 - 시작
+    func testConversionForFour() {
+        let result = converter.convert(4)
+        XCTAssertEqual(result, "IV", "Conversion for 4 is incorrect")
+    }
+    
+    func testConversionForNine() {
+        let result = converter.convert(9)
+        XCTAssertEqual(result, "IX", "Conversion for 9 is incorrect")
+    }
+    
+    func testConversionForZero() {
+        let result = converter.convert(0)
+        XCTAssertEqual(result, "", "Conversion for 0 is incorrect")
+    }
+    // 특별한 케이스 - 종료
+    
+    func testConversionFor3999() {
+        let result = converter.convert(3999)
+        XCTAssertEqual(result, "MMMCMXCIX", "Conversion for 3999 is incorrect")
     }
 }
